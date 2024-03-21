b4c419d0f4bae02af631fe1c526eade69d674c02
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-03-21 10:16:25 +0800

    Create AddAreaEffectIfHasFact.cs

---
+++ Components/AddAreaEffectIfHasFact.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Kingmaker.Blueprints;
+using Kingmaker.UnitLogic;
+using Kingmaker.UnitLogic.Buffs.Components;
+using Kingmaker.UnitLogic.Class.LevelUp.Actions;
+using Kingmaker.UnitLogic.Class.LevelUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MythicMagicMayhem.Components
+{
+    internal class AddAreaEffectIfHasFact : AddAreaEffect
+    {
+        public BlueprintFeatureReference RequiredFact;
+    }
+
+
+    [HarmonyPatch(typeof(AddAreaEffect), nameof(AddAreaEffect.IsOwnerAvatarValid))]
+    internal static class AreaEffectIfHasFactPatch
+    {
+        private static void Postfix(ref AddAreaEffect __instance, ref bool __result)
+        {
+            if (__instance is AddAreaEffectIfHasFact eff && !__instance.Owner.HasFact(eff.RequiredFact))
+            {
+                __result = false;
+            }
+        }
+    }
+}
