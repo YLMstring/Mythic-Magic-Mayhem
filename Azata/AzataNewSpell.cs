ea4f788f1fef6181f5e9f7be28454979578c5899
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-06-19 13:44:58 +0800

    1

--- Azata/AzataNewSpell.cs
+++ Azata/AzataNewSpell.cs
@@ -9,18 +9,204 @@ using BlueprintCore.Utils.Types;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes.Spells;
 using Kingmaker.Craft;
+using Kingmaker.RuleSystem;
 using Kingmaker.UnitLogic.Abilities;
 using Kingmaker.UnitLogic.Abilities.Blueprints;
 using Kingmaker.UnitLogic.Abilities.Components;
 using Kingmaker.UnitLogic.Abilities.Components.Base;
 using Kingmaker.Utility;
+using MythicMagicMayhem.Aeon;
 using MythicMagicMayhem.Components;
+using MythicMagicMayhem.Demon;
 using static Kingmaker.Visual.Animation.Kingmaker.Actions.UnitAnimationActionCastSpell;
 
 namespace MythicMagicMayhem.Azata
 {
     internal class AzataNewSpell
     {
+        private const string DragonDanceAbility = "NewSpell.UseDragonDance";
+        public static readonly string DragonDanceAbilityGuid = "{9942707F-4003-4846-B4DD-BFD46D1B67FD}";
+
+        private const string DragonDanceAbility1 = "NewSpell.UseDragonDance1";
+        public static readonly string DragonDanceAbilityGuid1 = "{2DFE0952-14DA-47DB-86E0-78520C593C69}";
+
+        private const string DragonDanceBuff1 = "NewSpell.DragonDanceBuff1";
+        private static readonly string DragonDanceBuffGuid1 = "{14130D2A-65EE-4367-B16D-635F704B6BF0}";
+
+        private const string DragonDanceAbility2 = "NewSpell.UseDragonDance2";
+        public static readonly string DragonDanceAbilityGuid2 = "{B4402763-C479-45B2-B149-87639EA2466A}";
+
+        private const string DragonDanceBuff2 = "NewSpell.DragonDanceBuff2";
+        private static readonly string DragonDanceBuffGuid2 = "{98966BAF-CA40-430F-A4E6-983C286C6FF4}";
+
+        private const string DragonDanceAbility3 = "NewSpell.UseDragonDance3";
+        public static readonly string DragonDanceAbilityGuid3 = "{7608AC8A-84E3-4A6F-ADF2-F5605F8F40AB}";
+
+        private const string DragonDanceBuff3 = "NewSpell.DragonDanceBuff3";
+        private static readonly string DragonDanceBuffGuid3 = "{68DB4241-4E7F-4029-B9C5-2E38CD8678D8}";
+
+        private const string DragonDanceAbility4 = "NewSpell.UseDragonDance4";
+        public static readonly string DragonDanceAbilityGuid4 = "{8F9BAAE7-A3CD-4438-860D-6CD5F5381553}";
+
+        private const string DragonDanceBuff4 = "NewSpell.DragonDanceBuff4";
+        private static readonly string DragonDanceBuffGuid4 = "{34DB17F5-507D-48F5-844A-A8E9B0A6D835}";
+
+        private const string DragonDanceAbility5 = "NewSpell.UseDragonDance5";
+        public static readonly string DragonDanceAbilityGuid5 = "{0C70A2EA-31C2-4EE1-8AF9-D19EEBCB5FC9}";
+
+        private const string DragonDanceBuff5 = "NewSpell.DragonDanceBuff5";
+        private static readonly string DragonDanceBuffGuid5 = "{4E695553-72E0-4E7B-BDC4-B8CBD9A32B4C}";
+
+        internal const string DisplayName10 = "NewSpellDragonDance.Name";
+        private const string Description10 = "NewSpellDragonDance.Description";
+
+        internal const string DisplayName11 = "NewSpellDragonDance1.Name";
+        private const string Description11 = "NewSpellDragonDance1.Description";
+
+        internal const string DisplayName12 = "NewSpellDragonDance2.Name";
+        private const string Description12 = "NewSpellDragonDance2.Description";
+
+        internal const string DisplayName13 = "NewSpellDragonDance3.Name";
+        private const string Description13 = "NewSpellDragonDance3.Description";
+
+        internal const string DisplayName14 = "NewSpellDragonDance4.Name";
+        private const string Description14 = "NewSpellDragonDance4.Description";
+
+        internal const string DisplayName15 = "NewSpellDragonDance5.Name";
+        private const string Description15 = "NewSpellDragonDance5.Description";
+        public static BlueprintAbility DragonDanceConfigure()
+        {
+            var icon = AbilityRefs.DragonAzataBuffAlliesAbility.Reference.Get().Icon;
+
+            var buff1 = BuffConfigurator.New(DragonDanceBuff1, DragonDanceBuffGuid1)
+              .SetDisplayName(DisplayName11)
+              .SetDescription(Description11)
+              .SetIcon(icon)
+              .AddEnergyVulnerability(Kingmaker.Enums.Damage.DamageEnergyType.Fire)
+              .Configure();
+
+            var ability1 = AbilityConfigurator.NewSpell(DragonDanceAbility1, DragonDanceAbilityGuid1, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName11)
+              .SetDescription(Description11)
+              .SetIcon(icon)
+              .SetLocalizedDuration(Duration.OneMinute)
+              .SetRange(AbilityRange.Long)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityDeliverChain(projectile: ProjectileRefs.ArrowFire00.ToString(), projectileFirst: ProjectileRefs.ArrowFire00.ToString(), radius: 30.Feet(), targetDead: false, targetsCount: ContextValues.Rank(), targetType: TargetType.Enemy, usedTargetsAgain: false, ignoreFirst: false)
+              .AddContextRankConfig(ContextRankConfigs.CasterLevel())
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff1, ContextDuration.Fixed(10))
+                  .DealDamage(value: ContextDice.Value(DiceType.D6, bonus: 0, diceCount: ContextValues.Rank()), damageType: DamageTypes.Energy(type: Kingmaker.Enums.Damage.DamageEnergyType.Fire), halfIfSaved: true)
+                  .Build(), savingThrowType: Kingmaker.EntitySystem.Stats.SavingThrowType.Reflex)
+              .Configure();
+
+            var buff2 = BuffConfigurator.New(DragonDanceBuff2, DragonDanceBuffGuid2)
+              .SetDisplayName(DisplayName12)
+              .SetDescription(Description12)
+              .SetIcon(icon)
+              .AddEnergyVulnerability(Kingmaker.Enums.Damage.DamageEnergyType.Cold)
+              .Configure();
+
+            var ability2 = AbilityConfigurator.NewSpell(DragonDanceAbility2, DragonDanceAbilityGuid2, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName12)
+              .SetDescription(Description12)
+              .SetIcon(icon)
+              .SetLocalizedDuration(Duration.OneMinute)
+              .SetRange(AbilityRange.Long)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityDeliverChain(projectile: ProjectileRefs.ColdCommonProjectile00.ToString(), projectileFirst: ProjectileRefs.ColdCommonProjectile00.ToString(), radius: 30.Feet(), targetDead: false, targetsCount: ContextValues.Rank(), targetType: TargetType.Enemy, usedTargetsAgain: false, ignoreFirst: false)
+              .AddContextRankConfig(ContextRankConfigs.CasterLevel())
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff2, ContextDuration.Fixed(10))
+                  .DealDamage(value: ContextDice.Value(DiceType.D6, bonus: 0, diceCount: ContextValues.Rank()), damageType: DamageTypes.Energy(type: Kingmaker.Enums.Damage.DamageEnergyType.Cold), halfIfSaved: true)
+                  .Build(), savingThrowType: Kingmaker.EntitySystem.Stats.SavingThrowType.Reflex)
+              .Configure();
+
+            var buff3 = BuffConfigurator.New(DragonDanceBuff3, DragonDanceBuffGuid3)
+              .SetDisplayName(DisplayName13)
+              .SetDescription(Description13)
+              .SetIcon(icon)
+              .AddEnergyVulnerability(Kingmaker.Enums.Damage.DamageEnergyType.Electricity)
+              .Configure();
+
+            var ability3 = AbilityConfigurator.NewSpell(DragonDanceAbility3, DragonDanceAbilityGuid3, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName13)
+              .SetDescription(Description13)
+              .SetIcon(icon)
+              .SetLocalizedDuration(Duration.OneMinute)
+              .SetRange(AbilityRange.Long)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityDeliverChain(projectile: ProjectileRefs.ElectroCommonProjectile00.ToString(), projectileFirst: ProjectileRefs.ElectroCommonProjectile00.ToString(), radius: 30.Feet(), targetDead: false, targetsCount: ContextValues.Rank(), targetType: TargetType.Enemy, usedTargetsAgain: false, ignoreFirst: false)
+              .AddContextRankConfig(ContextRankConfigs.CasterLevel())
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff1, ContextDuration.Fixed(10))
+                  .DealDamage(value: ContextDice.Value(DiceType.D6, bonus: 0, diceCount: ContextValues.Rank()), damageType: DamageTypes.Energy(type: Kingmaker.Enums.Damage.DamageEnergyType.Electricity), halfIfSaved: true)
+                  .Build(), savingThrowType: Kingmaker.EntitySystem.Stats.SavingThrowType.Reflex)
+              .Configure();
+
+            var buff4 = BuffConfigurator.New(DragonDanceBuff4, DragonDanceBuffGuid4)
+              .SetDisplayName(DisplayName14)
+              .SetDescription(Description14)
+              .SetIcon(icon)
+              .AddEnergyVulnerability(Kingmaker.Enums.Damage.DamageEnergyType.Acid)
+              .Configure();
+
+            var ability4 = AbilityConfigurator.NewSpell(DragonDanceAbility4, DragonDanceAbilityGuid4, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName14)
+              .SetDescription(Description14)
+              .SetIcon(icon)
+              .SetLocalizedDuration(Duration.OneMinute)
+              .SetRange(AbilityRange.Long)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityDeliverChain(projectile: ProjectileRefs.AcidArrow00.ToString(), projectileFirst: ProjectileRefs.AcidArrow00.ToString(), radius: 30.Feet(), targetDead: false, targetsCount: ContextValues.Rank(), targetType: TargetType.Enemy, usedTargetsAgain: false, ignoreFirst: false)
+              .AddContextRankConfig(ContextRankConfigs.CasterLevel())
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff1, ContextDuration.Fixed(10))
+                  .DealDamage(value: ContextDice.Value(DiceType.D6, bonus: 0, diceCount: ContextValues.Rank()), damageType: DamageTypes.Energy(type: Kingmaker.Enums.Damage.DamageEnergyType.Acid), halfIfSaved: true)
+                  .Build(), savingThrowType: Kingmaker.EntitySystem.Stats.SavingThrowType.Reflex)
+              .Configure();
+
+            var buff5 = BuffConfigurator.New(DragonDanceBuff5, DragonDanceBuffGuid5)
+              .SetDisplayName(DisplayName15)
+              .SetDescription(Description15)
+              .SetIcon(icon)
+              .AddEnergyVulnerability(Kingmaker.Enums.Damage.DamageEnergyType.Sonic)
+              .Configure();
+
+            var ability5 = AbilityConfigurator.NewSpell(DragonDanceAbility5, DragonDanceAbilityGuid5, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName15)
+              .SetDescription(Description15)
+              .SetIcon(icon)
+              .SetLocalizedDuration(Duration.OneMinute)
+              .SetRange(AbilityRange.Long)
+              .SetType(AbilityType.Spell)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityDeliverChain(projectile: ProjectileRefs.SonicCommonRay00_Projectile.ToString(), projectileFirst: ProjectileRefs.SonicCommonRay00_Projectile.ToString(), radius: 30.Feet(), targetDead: false, targetsCount: ContextValues.Rank(), targetType: TargetType.Enemy, usedTargetsAgain: false, ignoreFirst: false)
+              .AddContextRankConfig(ContextRankConfigs.CasterLevel())
+              .AddAbilityEffectRunAction(
+                actions: ActionsBuilder.New()
+                  .ApplyBuff(buff1, ContextDuration.Fixed(10))
+                  .DealDamage(value: ContextDice.Value(DiceType.D6, bonus: 0, diceCount: ContextValues.Rank()), damageType: DamageTypes.Energy(type: Kingmaker.Enums.Damage.DamageEnergyType.Sonic), halfIfSaved: true)
+                  .Build(), savingThrowType: Kingmaker.EntitySystem.Stats.SavingThrowType.Reflex)
+              .Configure();
+
+            return AbilityConfigurator.NewSpell(DragonDanceAbility, DragonDanceAbilityGuid, SpellSchool.Evocation, canSpecialize: true)
+              .SetDisplayName(DisplayName10)
+              .SetDescription(Description10)
+              .SetIcon(icon)
+              .SetAvailableMetamagic(Metamagic.CompletelyNormal, Metamagic.Empower, Metamagic.Quicken, Metamagic.Maximize)
+              .AddAbilityVariants([ability1, ability2, ability3, ability4, ability5])
+              .Configure();
+        }
+
         private const string GroupHugAbility1 = "NewSpell.UseGroupHug1";
         public static readonly string GroupHugAbility1Guid = "{D12E2DFB-D841-4BA0-893B-2BE19C535DDC}";
 
